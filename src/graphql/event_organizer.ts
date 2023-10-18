dc92601fdb3637eae52a0b5b7ffe3a4d64c0459b
Anil Khadka <anil.khadka3777@gmail.com>
2023-10-18 10:07:30 +0545

    Integrated organizers tablke

--- src/graphql/event_organizer.ts
+++ src/graphql/event_organizer.ts
@@ -1,13 +1,28 @@
 import { gql } from "@apollo/client";
 
-
-
 export const CREATE_EVENT_ORGANIZER_MUTATION = gql`
   mutation createEventOrganizer($eventId: Int!, $userId: Int!, $roleId: Int!) {
-    createEventOrganizer(eventId: $eventId, input : {
-        eventId : $eventId,
-        userId : $userId,
-        roleId : $roleId,
-    })
+    createEventOrganizer(
+      eventId: $eventId
+      input: { eventId: $eventId, userId: $userId, roleId: $roleId }
+    )
+  }
+`;
+
+export const GET_EVENT_ORGANIZERS_BY_EVENT_QUERY = gql`
+  query GetEventOrganizersByEvent($eventId: Int!) {
+    getEventOrganizers(eventId: $eventId) {
+      eventOrganizerId,
+      eventId {
+        eventId
+      },
+      userId {
+        userId,
+        name
+      },
+      roleId {
+        roleName
+      }
+    }
   }
 `;

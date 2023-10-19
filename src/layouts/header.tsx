63e433dc97ebfd989bd2ecf0f27d5b317e756fd8
Anil Khadka <anil.khadka3777@gmail.com>
2023-10-19 13:43:01 +0545

    Redirect to dashboard on logo click

--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
   return (
     <header className="bg-blue-500 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <Link href="/">
+        <Link href="/dashboard">
           Your Logo
         </Link>
         <nav className="space-x-4">
